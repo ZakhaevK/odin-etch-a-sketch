9dc032d841894e71d493b1b0d1262eecc6261a63
ZakhaevK <zkupke@gmail.com>
2024-06-12 18:56:23 +1000

    Add mouseover of square event to change color Includes functions randomRGBValues() and modified InitialiseGridElements()

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const grid = document.querySelector(".grid-div");
 let squareArray = []; // Used to access squares later (might change)
 
+function randomRGBValues() {
+  let color1 = (Math.random() * 255).toFixed(0);
+  let color2 = (Math.random() * 255).toFixed(0);
+  let color3 = (Math.random() * 255).toFixed(0);
+  
+  return `${color1}, ${color2}, ${color3}`
+}
+
 function initialiseGridElements(num) {
   for (let i = 0; i < num; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
     squareArray[i] = square;
     grid.appendChild(square);
+    
+    square.addEventListener("mouseover", () => {
+      square.setAttribute("style", `background-color:rgb(${randomRGBValues()})`);
+    })
   }
  }
 
